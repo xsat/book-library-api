dc1a455372d2296615644e7b2891e44955729070
Yurii Antonchyk <xsat.php@gmail.com>
2023-12-17 21:34:45 +0200

    Add book and author routes

--- app.py
+++ app.py
@@ -1,8 +1,74 @@
 from flask import Flask
 
-app = Flask(__name__)
+
+app: Flask = Flask(__name__)
 
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+@app.route("/books", methods=["GET"])
+def books_list_get() -> dict:
+    return {
+        "list": []
+    }
+
+
+@app.route("/books", methods=["POST"])
+def books_list_post() -> dict:
+    return {
+        "book_id": 1
+    }
+
+
+@app.route("/books/<int:book_id>", methods=["GET"])
+def books_item_get(book_id: int) -> dict:
+    return {
+        "book_id": book_id
+    }
+
+
+@app.route("/books/<int:book_id>", methods=["PUT"])
+def books_item_put(book_id: int) -> dict:
+    return {
+        "book_id": book_id
+    }
+
+
+@app.route("/books/<int:book_id>", methods=["DELETE"])
+def books_item_delete(book_id: int) -> dict:
+    return {
+        "book_id": book_id
+    }
+
+
+@app.route("/authors", methods=["GET"])
+def authors_list_get() -> dict:
+    return {
+        "list": []
+    }
+
+
+@app.route("/authors", methods=["POST"])
+def authors_list_post() -> dict:
+    return {
+        "book_id": 1
+    }
+
+
+@app.route("/authors/<int:author_id>", methods=["GET"])
+def authors_item_get(author_id: int) -> dict:
+    return {
+        "author_id": author_id
+    }
+
+
+@app.route("/authors/<int:author_id>", methods=["PUT"])
+def authors_item_put(author_id: int) -> dict:
+    return {
+        "author_id": author_id
+    }
+
+
+@app.route("/authors/<int:author_id>", methods=["DELETE"])
+def authors_item_delete(author_id: int) -> dict:
+    return {
+        "author_id": author_id
+    }
