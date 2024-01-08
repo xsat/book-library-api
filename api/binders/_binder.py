70a4f1a1f69118b4f5b778141cf50021c1f81268
Yurii Antonchyk <xsat.php@gmail.com>
2024-01-08 14:09:51 +0200

    Strip input values from requests

--- api/binders/_binder.py
+++ api/binders/_binder.py
@@ -3,11 +3,21 @@ from flask import Request
 from typing import final, Any
 
 
+def _prepare_value(value: Any) -> Any:
+    if isinstance(value, str):
+        return value.strip()
+
+    return value
+
+
 class Binder:
     @final
     def __init__(self, request: Request) -> None:
         json: Any = request.get_json(silent=True)
         if isinstance(json, dict):
+            for key, value in json.items():
+                json[key] = _prepare_value(value)
+
             self._assign(json)
         else:
             self._assign({})
