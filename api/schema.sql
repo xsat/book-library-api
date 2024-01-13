c6f5ca8a714b0a4ee4a201ff47e8232303dc8f47
Yurii Antonchyk <xsat.php@gmail.com>
2024-01-13 17:55:10 +0200

    Remove published_at in book table

--- api/schema.sql
+++ api/schema.sql
@@ -34,7 +34,6 @@ CREATE TABLE `books` (
     `user_id` int(11) unsigned NOT NULL,
     `title` varchar(255) NOT NULL,
     `pages` int(11) unsigned NOT NULL DEFAULT 0,
-    `published_at` date DEFAULT NULL,
     `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
     PRIMARY KEY (`book_id`),
     UNIQUE KEY `books_book_id_uk` (`book_id`),
