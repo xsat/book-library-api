a1894d07574fa4a6e1cd9b40e8385a82513e740f
Yurii Antonchyk <xsat.php@gmail.com>
2024-01-14 09:52:52 +0200

    Extend user mapper

--- api/mappers/users_mapper.py
+++ api/mappers/users_mapper.py
@@ -1,6 +1,8 @@
-from ..db import query_one
+from ..db import query_one, execute
+from ..hash import password_generate
 from ..datetime import today_datetime, to_mysql_datetime
 from ..models.user import User
+from ..binders.user_binder import UserBinder
 
 
 def user_find_by_username(username: str) -> User | None:
@@ -32,6 +34,38 @@ def user_find_by_valid_access_token(access_token: str) -> User | None:
         return _build_user_from_dict(result)
 
     return None
+
+
+def user_create_by_binder(user_binder: UserBinder) -> User:
+    password_hash: str = password_generate(user_binder.password)
+    user_id: int | None = execute(
+        "INSERT INTO `users` (`username`, `password_hash`) " +
+        "VALUE (%s, %s)",
+        (user_binder.username, password_hash)
+    )
+
+    return User(user_id, user_binder.username, password_hash)
+
+
+def user_update_by_binder(user: User, user_binder: UserBinder) -> None:
+    password_hash: str = password_generate(user_binder.password)
+    updated_user: User = User(user.user_id, user_binder.username, password_hash)
+    user.assign(updated_user)
+
+    execute(
+        "UPDATE `users` AS u " +
+        "SET u.`name` = %s, u.`password_hash` = %s " +
+        "WHERE u.`user_id` = %s",
+        (user.username, password_hash, user.user_id)
+    )
+
+
+def user_delete(user: User) -> None:
+    execute(
+        "DELETE FROM `users` AS u " +
+        "WHERE u.`user_id` = %s",
+        (user.user_id, )
+    )
 
 
 def _build_user_from_dict(data: dict) -> User:
